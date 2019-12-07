499c7ac4c2520691e651912bd1f52b174bc397b0
eureka84 <angelosciarra@ymail.com>
2019-12-07 11:51:17 +0100

    minor

--- src/test/kotlin/com/eureka/OpticsTest.kt
+++ src/test/kotlin/com/eureka/OpticsTest.kt
@@ -10,13 +10,13 @@ class OpticsTest {
 
     @Test
     fun test() {
-        val flight = Flight(Leg(Airport("MXP"), Airport("AMS")))
+        val flight = Flight(outbound = Leg(departure = Airport("MXP"), arrival = Airport("AMS")))
 
         val outboundDepartureName: Lens<Flight, String> = Flight.outbound.departure.name
 
         Assert.assertThat(
             outboundDepartureName.modify(flight) { "BGY" },
-            CoreMatchers.equalTo(Flight(Leg(Airport("BGY"), Airport("AMS"))))
+            CoreMatchers.equalTo(Flight(outbound = Leg(departure = Airport("BGY"), arrival = Airport("AMS"))))
         )
     }
 }
