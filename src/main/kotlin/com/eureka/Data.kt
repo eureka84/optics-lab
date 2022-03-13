2e89d9e2e8a3927e8bbfff3be4b5b3d8c551317e
Angelo Sciarra <angelosciarra@ymail.com>
2022-03-13 22:09:20 +0100

    test with list optics

--- src/main/kotlin/com/eureka/Data.kt
+++ src/main/kotlin/com/eureka/Data.kt
@@ -1,8 +1,10 @@
 package com.eureka
 
 import arrow.optics.optics
+import java.time.Duration
 
 
 @optics data class Airport(val name: String) { companion object }
-@optics data class Leg(val departure: Airport, val arrival: Airport) { companion object }
+@optics data class Leg(val departure: Airport, val arrival: Airport, val duration: Duration) { companion object }
 @optics data class Flight(val outbound: Leg, val inbound: Leg? = null) { companion object }
+@optics data class Trip(val flights: List<Flight>) { companion object }
--- src/test/kotlin/com/eureka/OpticsTest.kt
+++ src/test/kotlin/com/eureka/OpticsTest.kt
@@ -1,22 +1,51 @@
 package com.eureka
 
 import arrow.optics.Lens
-import org.hamcrest.CoreMatchers
-import org.junit.Assert
+import arrow.optics.PEvery
+import arrow.optics.dsl.every
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertThat
 import org.junit.Test
+import java.time.Duration
 
 
 class OpticsTest {
 
+    private val flight = Flight(
+        outbound = Leg(
+            departure = Airport("MXP"),
+            arrival = Airport("AMS"),
+            duration = Duration.ofHours(2)
+        )
+    )
+
     @Test
     fun test() {
-        val flight = Flight(outbound = Leg(departure = Airport("MXP"), arrival = Airport("AMS")))
-
         val outboundDepartureName: Lens<Flight, String> = Flight.outbound.departure.name
-
-        Assert.assertThat(
+        assertThat(
             outboundDepartureName.modify(flight) { "BGY" },
-            CoreMatchers.equalTo(Flight(outbound = Leg(departure = Airport("BGY"), arrival = Airport("AMS"))))
+            equalTo(
+                Flight(
+                    outbound = Leg(
+                        departure = Airport("BGY"),
+                        arrival = Airport("AMS"),
+                        duration = Duration.ofHours(2)
+                    )
+                )
+            )
         )
+    }
+
+    @Test
+    fun test2() {
+        val trip = Trip(listOf(flight))
+
+        val durationOvEveryOutboundFlight = Trip.flights.every(PEvery.list()).outbound.duration
+
+        val modifiedTrip = durationOvEveryOutboundFlight.modify(trip) { originalDuration ->
+            originalDuration.plus(Duration.ofMinutes(30))
+        }
+
+        assertThat(modifiedTrip.flights[0].outbound.duration, equalTo(Duration.ofMinutes(150)))
     }
 }
